e973542cc89af7e2e2a76a41b669a4d73a8ec4b8
Dave Gardyasz <david@everwash.com>
2021-05-28 17:16:51 -0400

    Changes to Server

--- src/infrastructure/Server.ts
+++ src/infrastructure/Server.ts
@@ -29,7 +29,7 @@ export default class Server {
         const file = path.join(staticPath, req.path + '.html');
         if (fs.existsSync(file) === true)
           req.url += '.html'
-          
+
         next();
       } else
         next();
@@ -44,22 +44,32 @@ export default class Server {
 
   onRequestStart = (request: core.Request, response: core.Response, next: core.NextFunction) => {
     // Here we can log usage analytics to see what is being called.
+    const staticPath = path.join(__dirname, '../../../web');
+
+    let url = request.originalUrl;
+    if (request.originalUrl.indexOf('?') > 0)
+      url = url.substr(0, request.originalUrl.indexOf('?'));
+
+    const file = path.join(staticPath, url);
     const type = chalk.blue('REQUEST');
     const method = chalk.yellow(request.method);
-    const url = chalk.white(request.originalUrl);
-    console.log(`${type}\t${method}\t${url}`);
+
+    if (fs.existsSync(file) === true) {
+      console.log(`${type}\t${method}\t${chalk.green(url)}`);
+    } else {
+      console.log(`${type}\t${method}\t${chalk.red(url)}`);
+    }
 
     next();
   }
 
 
   listen = (port: number) => {
+    this.app.use(this.onRequestStart);
     this.middleware();
 
     const router = express.Router();
 
-    // Order Matters!
-    this.app.use(this.onRequestStart);
     this.app.use('/', router);
 
 
